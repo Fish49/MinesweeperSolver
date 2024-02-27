f93a1ecc8c6b8a2f20c8542de23ec6d578e6aaaf
Joseph Calabro <beebfish49@gmail.com>
2024-02-26 22:51:14 -0700

    i wrote a ton of the code for minesweeperprofileadder on my local pc where i can test it then i just copied it over. still needs some work but right now it can find origin, squaresize, and board size.

--- MinesweeperProfileAdder.py
+++ MinesweeperProfileAdder.py
@@ -1,6 +1,8 @@
 import pynput
 from PIL import Image, ImageGrab
 import keyboard
+import threading
+import pygetwindow
 
 profile={
     "originPoint":[0, 0],
@@ -21,8 +23,128 @@ profile={
     "minSimilarity":10
 }
 
-def whenKeyboardPress(KEY):
-    print(KEY)
+mouse = pynput.mouse.Controller()
+vscode = pygetwindow.getActiveWindow()
 
-mouse=pynput.mouse.Controller()
-keyboard.on_press()
+def focus():
+    print('click on your minesweeper window to bring it into focus. make sure that you are able to see the vscode window the entire time')
+    print('when youre minesweeper window is focused, click C to start the process.')
+    while True:
+        event = keyboard.read_event()
+        if event.name == 'c':
+            break
+
+def getOriginPoint():
+    global profile
+
+    print('\nUse the wasd keys to navigate to the bottom-left corner of the top-left square on youre board. hit enter when youre done')
+
+    while True:
+        event = keyboard.read_event()
+        mousePos = list(mouse.position)
+
+        if event.event_type == 'up':
+            continue
+        if event.name == 'enter':
+            break
+
+        if event.name == 'w':
+            mousePos[1]-=1
+        if event.name == 's':
+            mousePos[1]+=1
+        if event.name == 'a':
+            mousePos[0]-=1
+        if event.name == 'd':
+            mousePos[0]+=1
+        mouse.position = mousePos
+
+    profile['originPoint']=mousePos
+
+def getSquareSize():
+    global profile
+    mouse.position=profile['originPoint']
+
+    print('\nUse the d key to move the mouse cursor to the leftmost pixel of the square to the right. hit enter when youre done')
+    print('if you accidentally move your mouse, hit R')
+
+    while True:
+        event = keyboard.read_event()
+        mousePos = list(mouse.position)
+
+        if event.event_type == 'up':
+            continue
+        if event.name == 'enter':
+            break
+
+        if event.name == 'd':
+            mousePos[0]+=1
+        if event.name == 'r':
+            mousePos = profile['originPoint']
+        mouse.position = mousePos
+
+    squareSize = mousePos[0]-profile["originPoint"][0]
+    profile['squareSize'] = squareSize
+
+def getBoardWidth():
+    global profile
+    mouse.position=profile['originPoint']
+
+    print('\nUse the d key to move the mouse cursor to the bottom-left pixel of the rightmost square. hit enter when youre done')
+    print('if you accidentally move your mouse, hit R')
+
+    boardWidth=1
+
+    while True:
+        event = keyboard.read_event()
+        mousePos = list(mouse.position)
+
+        if event.event_type == 'up':
+            continue
+        if event.name == 'enter':
+            break
+
+        if event.name == 'd':
+            mousePos[0] += profile['squareSize']
+            boardWidth += 1
+        if event.name == 'r':
+            mousePos = profile['originPoint']
+            boardWidth = 1
+
+        mouse.position = mousePos
+
+    profile['boardWidth'] = boardWidth
+
+def getBoardHeight():
+    global profile
+    mouse.position = profile['originPoint']
+
+    print('\nUse the s key to move the mouse cursor to the bottom-left pixel of the lowest square. hit enter when youre done')
+    print('if you accidentally move your mouse, hit R')
+
+    boardHeight=1
+
+    while True:
+        event = keyboard.read_event()
+        mousePos = list(mouse.position)
+
+        if event.event_type == 'up':
+            continue
+        if event.name == 'enter':
+            break
+
+        if event.name == 's':
+            mousePos[1]+=profile['squareSize']
+            boardHeight+=1
+        if event.name == 'r':
+            mousePos=profile['originPoint']
+            boardHeight=1
+
+        mouse.position = mousePos
+
+    profile['boardHeight'] = boardHeight
+
+focus()
+getOriginPoint()
+getSquareSize()
+getBoardWidth()
+getBoardHeight()
