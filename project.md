36b396448b8d05849bedfb594aa2d74921276fc5
Joseph Calabro <beebfish49@gmail.com>
2024-03-18 22:56:52 -0600

    getting started with the v5.0 changes.

--- project.md
+++ project.md
@@ -3,3 +3,4 @@
 ## TODO:
 - rewrite the code for v5.0
 - allow users to save custom profiles
+- show board feature that shows the board and its characteristics.
---
+++ v5/MinesweeperSolver.py
@@ -0,0 +1,19 @@
+'''
+MinesweeperSolver v5.0
+-PaiShoFish49
+'''
+
+from PIL import ImageGrab, Image, ImageTk, ImageDraw
+import pynput
+import threading
+import keyboard
+import math
+import json
+import time
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+class Board():
+    def __init__(self) -> None:
+        pass
